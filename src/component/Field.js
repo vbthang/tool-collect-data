3ca6187133b037f5a8ea1790a1b4e4dda87f6f7b
tphuyen <trinhhuyen.tph@gmail.com>
2024-01-30 11:26:14 +0700

    UPDATE: Create folder component & Field.js & Header.js

---
+++ src/component/Field.js
@@ -0,0 +1,11 @@
+import React from 'react'
+const Field = (props) => {
+  return (
+    <div className='field'>
+        <h3 className = 'label'>{props.name}</h3>
+        <input/>
+    </div>
+  )
+}
+
+export default Field
