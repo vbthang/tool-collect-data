a149a1110d1b90599ae138803e5534c53b311c59
tphuyen <trinhhuyen.tph@gmail.com>
2024-02-01 15:24:55 +0700

    UPDATE: responsive again

--- src/component/ChooseImage.js
+++ src/component/ChooseImage.js
@@ -5,7 +5,7 @@ const ChooseImage = () => {
   return (
     <div>
         <div className = 'image col-12 my-4'>
-            <FieldImage name='Front Image'/>
+            <FieldImage name='Front Image '/>
             <FieldImage name='Side Image'/>
             <FieldImage name='Back Image'/>
         </div>
--- src/component/Measurement.js
+++ src/component/Measurement.js
@@ -8,13 +8,13 @@ const Measurement = () => {
   return (
     <div className = 'detail col-md-8 col-12 my-4'>
         <h2 className='fs-4 bg-danger-subtle rounded p-2'>Volume:</h2>
-        <div className = 'volume row mb-4 px-5'>
+        <div className = 'volume row mb-4'>
           {volumeFields.map((field) => {
             return <Field key={field.id} id={field.id} name={field.name} placeholder={field.placeholder} require={field.require} col={`col-sm-6 col-12`}/>;
           })}
         </div>
         <h2 className='fs-4 bg-danger-subtle rounded p-2'>Linear:</h2>
-        <div className = 'linear row px-5'>
+        <div className = 'linear row'>
           {linearFields.map((field) => {
               return <Field key={field.id} id={field.id} name={field.name} placeholder={field.placeholder} require={field.require} col={`col-sm-6 col-12`}/>;
           })}
